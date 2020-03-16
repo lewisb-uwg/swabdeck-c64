a812dfd1c421ced87e0b67bdcaa1409c28bf9880
Lewis Baumstark <lewisb@westga.edu>
2020-03-16 16:00:43 -0400

    apparently you don't need to turn off interrupts if you're already using a raster-interrupted style...

--- main.asm
+++ main.asm
@@ -157,11 +157,11 @@ defm set_common_multicolor_sprite_colors
 *=$0810
 PROGRAM_START
         ; setup phase
-        jsr COPY_SCREEN_DATA_TO_SCREEN_RAM
-        jsr ENABLE_MULTICOLOR_CHAR_MODE
-        jsr SET_SHARED_SCREEN_COLORS
-        jsr REDIRECT_TO_CUSTOM_CHARSET
-        jsr APPLY_PER_CHAR_COLORS
+        ;jsr COPY_SCREEN_DATA_TO_SCREEN_RAM
+        ;jsr ENABLE_MULTICOLOR_CHAR_MODE
+        ;jsr SET_SHARED_SCREEN_COLORS
+        ;jsr REDIRECT_TO_CUSTOM_CHARSET
+        ;jsr APPLY_PER_CHAR_COLORS
         jsr INITIALIZE_PIRATE_SPRITE
         jsr INITIALIZE_SEAGULL_SPRITE
         jsr INITIALIZE_COCONUT_SPRITE
@@ -203,7 +203,7 @@ init_raster_interrupt
 
 main_game_loop
         ; update the pirate's location and animation
-        ;jsr UPDATE_PIRATE
+        jsr UPDATE_PIRATE
 
         ; update the seagull's location and animation
         jsr UPDATE_SEAGULL
@@ -233,36 +233,42 @@ main_game_loop
 ; X_INCR_VAL: the amount to increment x. 255 max (8-bit limit)
 ; SPRITE_MASK: bit set for the sprite getting incremented
 ; SPRITE_X_HI_TEMP: byte to hold the high bit ($D010-style) of the
-;                   sprite's x-coordinat. Set with the corresponding bit of $D010
+;                   sprite's x-coordinat. Initialize with $D010
 ; outputs:
 ; X_TEMP: is the new low byte of the caller's x-position
 ; SPRITE_X_HI_TEMP: appropriate sprite hi bit is set/unset as needed. It
 ;                   guarantees to preserve other sprites' hi bits, so it can
 ;                   be copied directly back to $D010 if needed.                   
 ADD_TO_X_COORDINATE
-        ldy #0
+        ; perform the addition on X_TEMP
+        clc
         lda X_TEMP
         adc X_INCR_VAL
-        sta X_TEMP ; note: does not reset carry flag
-        bcc @end ; if c=0, nothing more required
+        sta X_TEMP
 
-        ; c=1, we have to deal with the high bits
-        lda SPRITE_MASK
-        and SPRITE_X_HI_TEMP ; contains the hi bits of sprite x-locations
-        bne @clear_hi_bit
+        ; if no carry, we're done
+        bcs @carry_result
+        rts
 
-@set_hi_bit
+@carry_result
+        ; here we have to deal with the high bit
+        ; see if hi bit is set or not
         lda SPRITE_X_HI_TEMP
-        ora SPRITE_MASK
-        jmp @mod_hi_bit
+        and SPRITE_MASK
+        beq @hi_bit_zero
 
-@clear_hi_bit
+        ; hi bit is 1, unset it
         lda SPRITE_MASK
         invert_acc
         and SPRITE_X_HI_TEMP
-@mod_hi_bit    
         sta SPRITE_X_HI_TEMP
-@end    rts
+        rts
+        
+@hi_bit_zero
+        ; simply set the hi bit and we're done
+        lda SPRITE_X_HI_TEMP
+        ora SPRITE_MASK
+        rts
 
 INITIALIZE_COCONUT_SPRITE ; sprite 2
         ; set the coconut's 10 color
@@ -406,47 +412,35 @@ SET_SHARED_SCREEN_COLORS
 
 
 UPDATE_PIRATE
+        lda #0
+        sta X_INCR_VAL
         jsr DETERMINE_MOVEMENT_DISTANCE
         lda X_INCR_VAL
         beq @end ; return if X_INCR_VAL hasn't changed
         
         jsr MOVE_PIRATE
-        jsr ANIMATE_PIRATE
+        ;jsr ANIMATE_PIRATE
 @end    rts
 
 ; Polls keyboard and sets X_INCR_VAL based on key pressed
 ; if 'S' pressed -- X_INCR_VAL gets a positive value
 ; if 'A' pressed -- X_INCR_VAL gets a negative value
-; X_INCR_VAL will be zero if the distance moves beyond the pirate's bounds
-pirate_x_increment=5
+; does NOT perform any min/max clipping
+pirate_speed = 5
 DETERMINE_MOVEMENT_DISTANCE
-        lda #0 ; init X_INCR_VAL to zero
-        sta X_INCR_VAL
         jsr CHECK_FOR_S_KEY
         lda INPUT_FLAGS
-        cmp #%00000010
-        lda #pirate_x_increment
-        bne @end
+        and #%00000010
+        beq @end
         
-        ; check to see if we're beyond x-max for pirate
-        ; this means the 9th bit is set and pirate_x_ptr > 41
-        lda $D010
-        and #%00000001
-        beq @standard_increment ; 9th bit not set, we're done
+        ; S was pressed
+        lda #pirate_speed
+        sta X_INCR_VAL
+        rts
         
-        ; 9th bit is set, see if pirate_x_ptr+increment > 41
-        lda pirate_x_ptr
-        adc #pirate_x_increment
-        cmp #41
-        bpl @standard_increment ; we're <41 so no need to clip
-        lda #41
-        sbc pirate_x_ptr
+@end    lda #0
         sta X_INCR_VAL
-        jmp @end
-@standard_increment     
-        lda #pirate_x_increment
-        sta X_INCR_VAL
-@end    rts
+        rts
 
 ; Checks for press of the 'S' key
 ; input: none
@@ -461,7 +455,7 @@ CHECK_FOR_S_KEY
         lda #0
         sta INPUT_FLAGS
 
-        sei ; deactivate interrupts
+        ;sei ; deactivate interrupts
         lda #%11111111 ; make port A the outputs
         sta DDRA
         
@@ -477,8 +471,8 @@ CHECK_FOR_S_KEY
         lda #%00000010 ; set the bit indicating 'S' was pressed
         sta INPUT_FLAGS
 
-        cli ; reactive interrupts
-@end    rts
+@end    ;cli ; reactivate interrupts
+        rts
 
 MOVE_PIRATE
         lda LOOP_TICK
@@ -487,18 +481,50 @@ MOVE_PIRATE
         bne @end
                 
         ; perform the movement
-        ldy #0
-        lda #%00000001
+        lda #%00000001 ; set the SPRITE_MASK
         sta SPRITE_MASK
-        lda pirate_x_ptr
+
+        lda $D000 ; set X_TEMP
         sta X_TEMP
-        lda #1
-        sta X_INCR_VAL
+
+        ; X_INCR_VAL should already be set
+
+        ; set SPRITE_X_HI_TEMP  
+        lda $D010
+        sta SPRITE_X_HI_TEMP
+        
         jsr ADD_TO_X_COORDINATE
+
+        jsr CLIP_TO_PIRATE_X_MAX
+        jsr CLIP_TO_PIRATE_X_MIN
+
         lda X_TEMP
-        sta pirate_x_ptr
+        sta $D000 ; sprite 0 x low byte
+
+        lda SPRITE_X_HI_TEMP ; set the hi bit
+        sta $D010
 
 @end    rts
+
+pirate_x_low_byte_max = 41
+CLIP_TO_PIRATE_X_MAX
+        lda SPRITE_X_HI_TEMP
+        and #%00000001
+        beq @end ; not at max if hi bit not set
+
+        ; hi bit is set, see if low byte is < pirate_x_low_byte_max
+        lda X_TEMP
+        cmp #pirate_x_low_byte_max
+        bmi @end ; negative result means pirate_x_low_byte_max > X_TEMP, so done
+
+        ; clip X_TEMP to pirate_x_low_byte_max
+        lda #pirate_x_low_byte_max
+        sta X_TEMP
+
+@end    rts
+
+CLIP_TO_PIRATE_X_MIN
+        rts
 
 ANIMATE_PIRATE
         rts
